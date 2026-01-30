b3eb089d8b4f68eca4acedfe8dd4250fdc14fded
iannunez-max <iannumi24@alumnes.ilerna.com>
2026-01-30 20:06:45 +0100

    main nou

--- src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/main/Main.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/main/Main.java
@@ -1,39 +1,72 @@
-package es.ilerna.M0486.ra3.pt22.pt3.anotacions.main;
+package es.ilerna.M0486.ra3.pt2.main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-import es.ilerna.M0486.ra3.pt22.pt3.anotacions.Person;
-import es.ilerna.M0486.ra3.pt22.pt3.anotacions.Student;
+import es.ilerna.M0486.ra3.pt2.domain.Car;
+import es.ilerna.M0486.ra3.pt2.domain.Motorcycle;
+import es.ilerna.M0486.ra3.pt2.domain.Person;
+import es.ilerna.M0486.ra3.pt2.domain.Plane;
+import es.ilerna.M0486.ra3.pt2.domain.Student;
+import es.ilerna.M0486.ra3.pt2.domain.Teacher;
+import es.ilerna.M0486.ra3.pt2.domain.Vehicle;
 
 public class Main {
 
+	private static List<Person> people = new ArrayList<>();
+	private static List<Vehicle> vehicles = new ArrayList<>();
 	public static void main(String[] args) {
-	
-		Session session = null;
-		Transaction transaction = null;
+
 		Scanner sc = new Scanner(System.in);
+		int opcio;
+
+		do {
+			System.out.println("\n===== MENÚ PRINCIPAL =====");
+			System.out.println("1) Fase 1: Crear dades de prova");
+			System.out.println("2) Fase 2: Treure vehicles de persones");
+			System.out.println("3) Fase 3: Actualitzar un vehicle");
+			System.out.println("0) Sortir");
+			System.out.print("Escull una opció: ");
+
+			opcio = sc.nextInt();
+			sc.nextLine();
+
+			switch (opcio) {
+				case 1:
+					fase1();
+					break;
+				case 2:
+					fase2();
+					break;
+				case 3:
+					fase3();
+					break;
+				case 0:
+					System.out.println("Fins aviat!");
+					break;
+				default:
+					System.out.println("Opció incorrecta.");
+			}
+
+		} while (opcio != 0);
+
+		sc.close();
+		HibernateSession.getSessionFactory().close();
+	}
 	
-		try {
-			session = HibernateSession.getSessionFactory().openSession();
-			transaction = session.beginTransaction();
-			
-		} catch (Exception ex) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-			
-			ex.printStackTrace();
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-			
-			HibernateSession.getSessionFactory().close();
-			sc.close();
-		}
+	private static void fase1() {
+
+	}
+	
+	private static void fase2() {
+
+	}
+
+	private static void fase3() {
+
 	}
 }
